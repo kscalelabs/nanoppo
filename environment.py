77be6a10d7876ce30359bc231292ad7f002358f9
Isaac Light <wlight@emory.edu>
2024-07-25 23:45:39 -0400

    add comments

--- environment.py
+++ environment.py
@@ -3,6 +3,7 @@
 from typing import NotRequired, Tuple, TypedDict, Unpack
 
 import mujoco
+import numpy as np
 from brax import base
 from brax.envs.base import PipelineEnv, State
 from brax.io import mjcf
@@ -22,15 +23,30 @@ class HumanoidEnv(PipelineEnv):
         self,
         **kwargs: Unpack[EnvKwargs],
     ) -> None:
+        # load the model which determines the robot + environment,
+        # including the physics and observation/action spaces
         mj_model = mujoco.MjModel.from_xml_path("assets/stompylegs.xml")
+
+        # extracts qpos found in the xml file
+        # qpos[:7] is the root position and orientation
+        # qpos[7:] is the joint angles
         self.initial_qpos = jp.array(mj_model.keyframe("default").qpos)
+
+        # action size is the number of dof/movable-joints
         self._action_size = mj_model.nu
+
+        # create a brax system from the mj model
         sys = mjcf.load_model(mj_model)
-        # kwargs stuff
+
+        ## keyword args
         physics_steps_per_control_step = 4
+        # n_frames: the number of times to step the physics pipeline for each
+        # environment step
         kwargs["n_frames"] = kwargs.get("n_frames", physics_steps_per_control_step)
+        # backend: string specifying the physics pipeline
         kwargs["backend"] = "mjx"
 
+        # calls super to PipelineEnv, a API to drive a brax system for training and inference
         super().__init__(sys, **kwargs)
 
     def reset(self, rng: jp.ndarray) -> State:
@@ -40,9 +56,14 @@ class HumanoidEnv(PipelineEnv):
 
         # initialize mjx state
         mjx_state = self.pipeline_init(qpos, qvel)
-        obs = self.get_obs(mjx_state)
-        reward, done, zero = jp.zeros(3)
 
+        # get current position (qpos)
+        obs = self.get_obs(mjx_state)
+
+        # done -> 1.0 if env is terminated, 0.0 otherwise
+        reward, done = jp.zeros(2)
+
+        # metrics can be set up to track forward velocity, etc.
         metrics = {}
 
         return State(mjx_state, obs, reward, done, metrics)
@@ -50,11 +71,15 @@ class HumanoidEnv(PipelineEnv):
     def rewards(
         self, mjx_state: MjxState, action: jp.ndarray, next_mjx_state: MjxState
     ) -> Tuple[jp.ndarray, jp.ndarray]:
+        # if robot falls below min_z or above max_z, reward is 0, otherwise 5
+        # healthy is 1 if robot is within min_z and max_z, 0 otherwise
         min_z = -1.0
         max_z = 2.0
         healthy = jp.where(mjx_state.qpos[2] < min_z, 0.0, 1.0)
         healthy = jp.where(mjx_state.qpos[2] > max_z, 0.0, healthy)
         reward = jp.array(5.0) * healthy
+
+        # other rewards and termination conditions can be added here
 
         return reward, healthy
 
@@ -62,19 +87,24 @@ class HumanoidEnv(PipelineEnv):
         """Run one timestep of the environment's dynamics."""
         mjx_state = state.pipeline_state
 
+        # get current position (qpos)
         obs = self.get_obs(mjx_state)
 
         # action scaling
+        # NOTE: actions is position based, so we need to scale them to be deltas, then add delta
         action_scale = 2.5
         delta_action = action - mjx_state.qpos[7:]
         delta_action *= action_scale
         action = jp.array(delta_action + mjx_state.qpos[7:])
 
+        # step the physics
         next_mjx_state = self.pipeline_step(mjx_state, action)
+
         # rewards
         reward, healthy = self.rewards(mjx_state, action, next_mjx_state)
-
         done = 1.0 - healthy
+
+        # next state becomes current state
         return state.replace(pipeline_state=next_mjx_state, obs=obs, reward=reward, done=done)
 
     def get_obs(self, data: MjxState) -> jp.ndarray:
--- play.py
+++ play.py
@@ -12,12 +12,11 @@ from brax.mjx.base import State as mjxState
 from brax.training.acme import running_statistics
 from brax.training.agents.ppo import networks as ppo_networks
 from tqdm import tqdm
-
+from omegaconf import DictConfig
 from environment import HumanoidEnv
 from train import config
 
 InferenceFn = Callable[[jp.ndarray, jp.ndarray], tuple[jp.ndarray, jp.ndarray]]
-
 
 def mjx_rollout(
     env: mujoco.MjModel,
@@ -26,17 +25,21 @@ def mjx_rollout(
     render_every: int = 2,
     seed: int = 0,
 ) -> list[mjxState]:
+    # references step and reset functions from the environment
     reset_fn = j.jit(env.reset)
     step_fn = j.jit(env.step)
     inference_fn = j.jit(inference_fn)
     rng = j.random.PRNGKey(seed)
     state = reset_fn(rng)
+    # rollout: list of pipeline states of the policy rollout
     rollout = [state.pipeline_state]
     for i in tqdm(range(n_steps)):
         action_rng, rng = j.random.split(rng)
-        # NOTE: actions is position based:
+        # get postprocessed action from inference function
         action, _ = inference_fn(state.obs, action_rng)
+        # get next state
         state = step_fn(state, action)
+        # add state to rollout
         rollout.append(state.pipeline_state)
 
         if state.done:
@@ -54,12 +57,13 @@ def render_mjx_rollout(
     width: int = 320,
     height: int = 240,
 ) -> np.ndarray:
+    # this function takes the rollout states, and render and array of corresponding images
     rollout = mjx_rollout(env, inference_fn, n_steps, render_every, seed)
     images = env.render(rollout[::render_every], camera="side", width=width, height=height)
     return np.array(images)
 
 
-def play(config: dict[str, Any], n_steps: int, render_every: int, width: int, height: int) -> None:
+def play(config: DictConfig, n_steps: int, render_every: int, width: int, height: int) -> None:
     ## initialize and reset the environment
     env = HumanoidEnv()
     rng = j.random.PRNGKey(0)
@@ -70,6 +74,8 @@ def play(config: dict[str, Any], n_steps: int, render_every: int, width: int, he
     # params -> (processor_params, PolicyNetwork)
     params = (params[0], params[1].policy)
 
+    ## creating inference function
+    # normalize function for observations to improve model efficiency
     normalize = running_statistics.normalize
     policy_network = ppo_networks.make_ppo_networks(
         env.observation_size,
@@ -79,6 +85,8 @@ def play(config: dict[str, Any], n_steps: int, render_every: int, width: int, he
         value_hidden_layer_sizes=config["value_hidden_layer_sizes"],
     )
     inference_fn = ppo_networks.make_inference_fn(policy_network)(params)
+    # inference_fn input: observation, action_rng
+    # inference_fn output: action, additionally (log probs, raw actions)
 
     ## render the video's images
     images_thwc = render_mjx_rollout(env, inference_fn, n_steps, render_every, width=width, height=height)
@@ -90,4 +98,7 @@ def play(config: dict[str, Any], n_steps: int, render_every: int, width: int, he
 
 
 if __name__ == "__main__":
+    # python -m play
     play(config, n_steps=1000, render_every=2, width=640, height=480)
+    # run this script to play the trained agent in the environment
+
